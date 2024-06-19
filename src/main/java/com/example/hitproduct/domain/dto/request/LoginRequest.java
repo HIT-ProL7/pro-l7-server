e41f582e597a1c89772facfdcc6063413c894933
lehonganh0201 <le2960620@gmail.com>
2024-06-19 15:18:47 +0700

    add feat add_user

--- src/main/java/com/example/hitproduct/domain/dto/request/LoginRequest.java
+++ src/main/java/com/example/hitproduct/domain/dto/request/LoginRequest.java
@@ -7,6 +7,10 @@ package com.example.hitproduct.domain.dto.request;
  * @social Facebook: https://www.facebook.com/profile.php?id=100047152174225
  */
 
+import com.example.hitproduct.constant.ValidationMessage;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
@@ -16,6 +20,11 @@ import lombok.experimental.FieldDefaults;
 @NoArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class LoginRequest {
+    @NotEmpty(message = "This field cannot empty")
+    @Size(min = 10, message = ValidationMessage.Login.STUDENT_CODE_INVALID)
     String studentCode;
+
+    @NotEmpty(message = "This field cannot empty")
+    @Pattern(regexp = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$", message = ValidationMessage.Login.PASSWORD_INVALID)
     String password;
 }
