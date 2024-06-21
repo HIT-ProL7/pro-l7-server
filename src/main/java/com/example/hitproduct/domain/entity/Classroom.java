4a7b5cde99c2beeabc1dcb96b4c6df24593cf54e
lehonganh0201 <le2960620@gmail.com>
2024-06-21 11:14:00 +0700

    add entity Classroom

---
+++ src/main/java/com/example/hitproduct/domain/entity/Classroom.java
@@ -0,0 +1,43 @@
+package com.example.hitproduct.domain.entity;
+/*
+ * @author HongAnh
+ * @created 21 / 06 / 2024 - 11:09 AM
+ * @project pro-l7-server
+ * @social Github: https://github.com/lehonganh0201
+ * @social Facebook: https://www.facebook.com/profile.php?id=100047152174225
+ */
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.sql.Timestamp;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@Entity
+@Table(name = "roles")
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Classroom {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column(nullable = false)
+    String name;
+
+    String description;
+
+    String roadmap;
+
+    @CreationTimestamp
+    Timestamp createAt;
+
+    @UpdateTimestamp
+    Timestamp updatedAt;
+}
