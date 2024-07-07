cd3ab3e776a5bd7e535f3631e1d645703fbea7b3
lehonganh0201 <le2960620@gmail.com>
2024-07-07 23:42:00 +0700

    add configuration cort

---
+++ src/main/java/com/example/hitproduct/config/WebConfig.java
@@ -0,0 +1,24 @@
+package com.example.hitproduct.config;
+/*
+ * @author HongAnh
+ * @created 07 / 07 / 2024 - 11:40 PM
+ * @project pro-l7-server
+ * @social Github: https://github.com/lehonganh0201
+ * @social Facebook: https://www.facebook.com/profile.php?id=100047152174225
+ */
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowCredentials(true);
+    }
+}
