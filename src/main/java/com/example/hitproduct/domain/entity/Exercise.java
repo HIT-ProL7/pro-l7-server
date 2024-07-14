4414a99da5b031cec41fcee4fd52ee5141789a07
Nguyen Viet Hoang <hoangnv.swe@gmail.com>
2024-07-14 16:40:05 +0700

    Added tracking creation timestamp and updated timestamp

--- src/main/java/com/example/hitproduct/domain/entity/Exercise.java
+++ src/main/java/com/example/hitproduct/domain/entity/Exercise.java
@@ -5,6 +5,9 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import java.util.Date;
 
@@ -21,7 +24,9 @@ public class Exercise {
     private Integer id;
     private String  title;
     private String  content;
+    @CreationTimestamp
     private Date    createdAt;
+    @UpdateTimestamp
     private Date    updatedAt;
 
     @ManyToOne(fetch = FetchType.LAZY)
